b6a806fa0f0f89cfbdd97c47833a71d763ccced1
pravesh meena <pravesh.meena2892@gmail.com>
2024-04-13 08:12:09 +0530

    update

--- server/models/user.js
+++ server/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
 	email: { type: String, required: true },
 	password: { type: String, required: true },
 	verified: { type: Boolean, default: false },
+	
 });
 
 userSchema.methods.generateAuthToken = function () {
