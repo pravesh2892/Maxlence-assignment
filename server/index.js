e835baa40e397e1e74b139f3a2f17e7a1977eafe
pravesh meena <pravesh.meena2892@gmail.com>
2024-04-15 11:49:16 +0530

    UPDATE

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors(
     {
         origin: ["https://deploye-mern-auth.vercel.app"],
-        methods: [ "POST", "GET"],
+        methods: [ "POST", "GET","PUT", "DELETE"],
         credentials: true
     }
 ));
